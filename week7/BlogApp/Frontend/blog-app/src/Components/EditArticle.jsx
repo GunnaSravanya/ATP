d7fc167d8bc66b562fd9086476f96a17949688f5
Gunna Sravanya <gunnasravanya@gmail.com>
2026-04-24 20:11:37 +0530

    Update EditArticle.jsx

--- week7/BlogApp/Frontend/blog-app/src/Components/EditArticle.jsx
+++ week7/BlogApp/Frontend/blog-app/src/Components/EditArticle.jsx
@@ -42,7 +42,7 @@ function EditArticle() {
     modifiedArticle.articleId = article._id;
     //make PUT req to update article
     let res = await axios.put(
-      "http://localhost:4000/author-api/article",
+      "https://atp-1.onrender.com/author-api/article",
       modifiedArticle,
       { withCredentials: true },
     );
