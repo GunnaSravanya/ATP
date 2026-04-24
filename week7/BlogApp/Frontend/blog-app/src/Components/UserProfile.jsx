92b465331fe869355308f7ef8e37d27946d716d8
Gunna Sravanya <gunnasravanya@gmail.com>
2026-04-24 20:15:58 +0530

    Update UserProfile.jsx

--- week7/BlogApp/Frontend/blog-app/src/Components/UserProfile.jsx
+++ week7/BlogApp/Frontend/blog-app/src/Components/UserProfile.jsx
@@ -29,7 +29,7 @@ function UserProfile() {
       try {
         //read articles of all authors
         let res = await axios.get(
-          "http://localhost:4000/user-api/articles",
+          "https://atp-1.onrender.com/user-api/articles",
           { withCredentials: true },
         );
         //update articles state
