85989b1d10dddbbd2eae0f5a25c3737431775cd1
Gunna Sravanya <gunnasravanya@gmail.com>
2026-04-24 20:17:15 +0530

    Update API endpoint for article submission

--- week7/BlogApp/Frontend/blog-app/src/Components/WriteArticles.jsx
+++ week7/BlogApp/Frontend/blog-app/src/Components/WriteArticles.jsx
@@ -39,7 +39,7 @@ function WriteArticles() {
       setLoading(true);
       //make POST req to save new article
       let res = await axios.post(
-        "http://localhost:4000/author-api/article",
+        "https://atp-1.onrender.com/author-api/article",
         articleObj,
         { withCredentials: true },
       );
