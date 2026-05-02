682d9852700a7727a8a372e10a812d8fd8fdba6a
Gunna Sravanya <gunnasravanya@gmail.com>
2026-05-02 12:03:50 +0530

    Update Header.jsx

--- week7/BlogApp/Frontend/blog-app/src/Components/Header.jsx
+++ week7/BlogApp/Frontend/blog-app/src/Components/Header.jsx
@@ -1,28 +1,44 @@
-import React from 'react'
-import {NavLink} from 'react-router'
+import React from 'react';
+import { NavLink, useNavigate } from 'react-router';
 import { useAuth } from '../store/authStore';
 
 function Header() {
-  const {isAuthenticated}=useAuth((state)=>state)
+  const { isAuthenticated, logout } = useAuth((state) => state);
+  const navigate = useNavigate();
+
+  const handleLogout = async (e) => {
+    e.preventDefault(); // 🔥 stop NavLink default behavior
+    await logout();
+    navigate("/login");
+  };
+
   return (
     <div className="bg-blue-500 p-3">
       <ul className="flex justify-end gap-4 text-2xl text-white pr-3 ">
+        
         <li>
           <NavLink to="/">Home</NavLink>
         </li>
+
         <li>
           <NavLink to="/Register">Register</NavLink>
         </li>
-        {isAuthenticated===true?
-        (<li>
-          <NavLink to="/Login">Logout</NavLink>
-        </li>):
-        (<li>
-          <NavLink to="/Login">Login</NavLink>
-        </li>)}
+
+        {isAuthenticated ? (
+          <li>
+            <NavLink to="/Login" onClick={handleLogout}>
+              Logout
+            </NavLink>
+          </li>
+        ) : (
+          <li>
+            <NavLink to="/Login">Login</NavLink>
+          </li>
+        )}
+
       </ul>
     </div>
   );
 }
 
-export default Header
+export default Header;
